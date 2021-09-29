8dd234da8e83882cfc50949b907d5e2393e7d6ed
CamilaCode7 <CamilaCode7>
2021-09-29 11:26:05 -0300

    adicionando component Pokemon

--- my-pokedex/src/Pokemon.jsx
+++ my-pokedex/src/Pokemon.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
-class Image extends Component {
+class Pokemon extends Component {
   render() {
+    const { pokemon: { name, type, averageWeight, image } } = this.props;
     return (
-      <img src={this.props.source} alt={this.props.alternativeText} />
+      <div className="pokemon">
+        <div>
+          <h2>{ name }</h2>
+          <p>{ type }</p>
+        </div>
+        <p>{ `Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}` }</p>
+        <img src={ image } alt={ `${name} sprite` } />
+      </div>
     )
   }
 }
-export default Image
+
+export default Pokemon;
